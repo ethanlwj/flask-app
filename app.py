cd754157f97fb4423eecb90f3b0b9bc125bc3b52
ethanlwj <69135248+ethanlwj@users.noreply.github.com>
2020-09-07 17:49:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from flask import Flask, jsonify, request
 from tensorflow.keras.models import load_model
 import json
 import pickle
+import sklearn
 
 NN_model = load_model('NN_model.h5')
 KNN_model = pickle.load(open('KNN_model.pkl','rb'))
+Light_classification = pickle.load(open('Light_Classification.pkl','rb'))
 
 # app
 app = Flask(__name__)
@@ -30,12 +32,36 @@ def predict():
     result_scaling_factor = KNN_model.predict(data_df)
     
     result = result_NN*result_scaling_factor[0]
+    
+    result_light_classification = Light_classification.predict(df_scaled)[0]
+    
+    result_dict = {
+        'SPM' : result.tolist(),
+        'Light' : result_light_classification
+        }
 
-    # send back to browser
-    result = json.dumps(result.tolist())
+    result = json.dumps(result_dict)
     
     # return data
     return result
+
+@app.route('/LC', methods=['POST'])
+def classify_light():
+    data = request.get_json(force=True)
+    
+    # convert data into dataframe
+    data_df = pd.DataFrame(data)
+    
+    max_value = data_df.max(axis=1)
+    df_scaled = data_df.apply(lambda x: x/max_value)
+    
+    result_light_classification = Light_classification.predict(df_scaled)[0]
+    print(result_light_classification)
+    
+    result = json.dumps(result_light_classification)
+    
+    return result;
+
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
