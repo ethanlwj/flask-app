16f7f6c73be46e641f4fdf45b7c68d4fce45acd8
ethanlwj <69135248+ethanlwj@users.noreply.github.com>
2020-09-14 20:10:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import sklearn
 
 NN_model = load_model('NN_model.h5')
 KNN_model = pickle.load(open('KNN_model.pkl','rb'))
+NN_model_v2 = load_model('NN_model_v2.h5')
+KNN_model_v2 = pickle.load(open('KNN_model_v2.pkl','rb'))
 Light_classification = pickle.load(open('Light_Classification.pkl','rb'))
 
 # app
@@ -28,8 +30,8 @@ def predict():
     df_scaled = data_df.apply(lambda x: x/max_value)
     
     # predictions
-    result_NN = NN_model.predict(df_scaled)
-    result_scaling_factor = KNN_model.predict(data_df)
+    result_NN = NN_model_v2.predict(df_scaled)
+    result_scaling_factor = KNN_model_v2.predict(data_df)
     
     result = result_NN*result_scaling_factor[0]
     
